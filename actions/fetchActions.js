9c384c939be7f734d1441cc17c3c35a5934f58a3
Mikwiss <mikwiss00@gmail.com>
2018-02-07 13:05:35 +0100

    New action arta name

--- actions/fetchActions.js
+++ actions/fetchActions.js
@@ -6,6 +6,6 @@
  * @license MIT
  *
  */
-export const IS_FETCHING = '@@connect/IS_FETCHING';
-export const END_IS_FETCHING = '@@connect/END_IS_FETCHING';
-export const PARTIAL_END_IS_FETCHING = '@@connect/PARTIAL_END_IS_FETCHING';
+export const IS_FETCHING = '@@arta-middleware/IS_FETCHING';
+export const END_IS_FETCHING = '@@arta-middleware/END_IS_FETCHING';
+export const PARTIAL_END_IS_FETCHING = '@@arta-middleware/PARTIAL_END_IS_FETCHING';
